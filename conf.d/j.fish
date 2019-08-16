c9c698e94ea4305d5a7df4c54fbf309c832bc585
William Myers <mwilliammyers@gmail.com>
2019-08-16 14:20:28 -0600

    Update j.fish

--- conf.d/j.fish
+++ conf.d/j.fish
@@ -1,5 +1,5 @@
 if type -q 'fd'
 	set -l exclude "-E .git/ -E .Trash/ -E .trash/ -E '*Cache*/' -E '*cache*/' -E Applications/ -E Library/ -E 'Album Artwork/' -E .rustup/ -E registry/ -E .npm/ -E node_modules/ -E .node-gyp/ -E venv/ -E env/ -E .apm/ -E packages/ -E pack/ -E '*.xcodeproj/'"
-	set -q FZF_CD_COMMAND; or set -U FZF_CD_COMMAND "fd -Lt d $exclude"
-	set -q FZF_CD_WITH_HIDDEN_COMMAND; or set -U FZF_CD_WITH_HIDDEN_COMMAND "fd -Lt d --hidden $exclude"
+	set -q FZF_CD_COMMAND; or set -Ux FZF_CD_COMMAND "fd -Lt d $exclude"
+	set -q FZF_CD_WITH_HIDDEN_COMMAND; or set -Ux FZF_CD_WITH_HIDDEN_COMMAND "fd -Lt d --hidden $exclude"
 end
