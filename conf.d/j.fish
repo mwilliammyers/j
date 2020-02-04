7736bad55be0121f42200f840f97b0296af136c9
William Myers <mwilliammyers@gmail.com>
2020-02-03 18:38:40 -0700

    Fix missing quote

--- conf.d/j.fish
+++ conf.d/j.fish
@@ -1,5 +1,5 @@
 if type -q 'fd'
-    set -l exclude "-E .git/ -E '*target/ -E .Trash/ -E .trash/ -E '*Cache*/' -E '*cache*/' -E Applications/ -E Library/ -E 'Album Artwork/' -E .rustup/ -E registry/ -E .npm/ -E node_modules/ -E .node-gyp/ -E venv/ -E env/ -E .venv/ -E .env/ -E .apm/ -E packages/ -E pack/ -E '*.xcodeproj/'"
+    set -l exclude "-E .git/ -E '*target/' -E .Trash/ -E .trash/ -E '*Cache*/' -E '*cache*/' -E Applications/ -E Library/ -E 'Album Artwork/' -E .rustup/ -E registry/ -E .npm/ -E node_modules/ -E .node-gyp/ -E venv/ -E env/ -E .venv/ -E .env/ -E .apm/ -E packages/ -E pack/ -E '*.xcodeproj/'"
    
 	# TODO: should we set this too?
     set -q FZF_DEFAULT_COMMAND
