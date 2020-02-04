4fd7781995a48bef24f469a062f74fe5a005b9d7
William Myers <mwilliammyers@gmail.com>
2020-02-03 18:45:24 -0700

    Update j.fish

--- conf.d/j.fish
+++ conf.d/j.fish
@@ -1,13 +1,13 @@
 if type -q 'fd'
     set -l exclude "-E .git/ -E '*target/' -E .Trash/ -E .trash/ -E '*Cache*/' -E '*cache*/' -E Applications/ -E Library/ -E 'Album Artwork/' -E .rustup/ -E registry/ -E .npm/ -E node_modules/ -E .node-gyp/ -E venv/ -E env/ -E .venv/ -E .env/ -E .apm/ -E packages/ -E pack/ -E '*.xcodeproj/'"
-   
-	# TODO: should we set this too?
+
+    # TODO: should we set this too?
     set -q FZF_DEFAULT_COMMAND
-    or set -Ux FZF_DEFAULT_COMMAND "fd -t f $exclude"
-	
-	set -q FZF_CD_COMMAND
+    or set -Ux FZF_DEFAULT_COMMAND "fd -t f --hidden $exclude"
+
+    set -q FZF_CD_COMMAND
     or set -Ux FZF_CD_COMMAND "fd -Lt d $exclude"
-    
-	set -q FZF_CD_WITH_HIDDEN_COMMAND
+
+    set -q FZF_CD_WITH_HIDDEN_COMMAND
     or set -Ux FZF_CD_WITH_HIDDEN_COMMAND "fd -Lt d --hidden $exclude"
 end
