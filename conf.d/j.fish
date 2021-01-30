f3b9e98a3daa9080472f3a6c4de0d3023e266e91
William Myers <mwilliammyers@gmail.com>
2021-01-29 21:00:40 -0800

    Update j.fish

--- conf.d/j.fish
+++ conf.d/j.fish
@@ -1,5 +1,5 @@
 set -q FZF_DEFAULT_OPTS
-or set -Ux FZF_DEFAULT_OPTS '--cycle --layout=reverse --height=33% --preview-window=wrap --marker="*"'
+or set -Ux FZF_DEFAULT_OPTS '--layout=reverse --height=33% --preview-window=wrap --marker="*"'
 
 if type -q 'fd'
     set -l exclude "-E .git/ -E '*target/' -E .Trash/ -E .trash/ -E '*Cache*/' -E '*cache*/' -E Applications/ -E Library/ -E 'Album Artwork/' -E .rustup/ -E registry/ -E .npm/ -E node_modules/ -E .node-gyp/ -E venv/ -E env/ -E .venv/ -E .env/ -E .apm/ -E packages/ -E pack/ -E '*.xcodeproj/' -E '*.theater/' -E '*.*library/'"
