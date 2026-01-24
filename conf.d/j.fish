cf8fd2fad7d7e8085873f8d56eba5013edb035b4
Will Myers <will@withmartian.com>
2026-01-23 22:41:06 -0800

    Update FZF_ env vars

--- conf.d/j.fish
+++ conf.d/j.fish
@@ -1,31 +1,28 @@
 # FZF default options
+# TODO: This should probably be set elsewhere
 set -gx FZF_DEFAULT_OPTS '--layout=reverse --height=33% --preview-window=wrap --marker="*"'
 
+# Shared fd exclusions for fzf
+# TODO: This should maybe be set elsewhere
+set -l fzf_exclude \
+    -E .git/ -E .git-worktrees/ \
+    -E .Trash/ -E .trash/ -E Applications/ -E Library/ \
+    -E '*Cache*/' -E '*cache*/' \
+    -E dist/ -E build/ \
+    -E .npm/ -E node_modules/ -E .node-gyp/ -E .apm/ \
+    -E venv/ -E env/ -E .venv/ -E __pycache__/ -E .mypy_cache/ -E .pytest_cache/ -E .ruff_cache/ -E '*.egg-info/' \
+    -E .cargo/ -E '*target/' \
+    -E '*.xcodeproj/' \
+    -E vendor/
+
+# For fzf.vim :Files command
 if type -q fd
-    set -l exclude "\
--E .git/ -E .git-worktrees/ \
--E .Trash/ -E .trash/ -E Applications/ -E Library/ \
--E '*Cache*/' -E '*cache*/' \
--E dist/ -E build/ \
--E .npm/ -E node_modules/ -E .node-gyp/ -E .apm/ \
--E venv/ -E env/ -E .venv/ -E __pycache__/ -E .mypy_cache/ -E .pytest_cache/ -E .ruff_cache/ -E '*.egg-info/' \
--E .cargo/ -E '*target/' \
--E '*.xcodeproj/' \
--E vendor/"
-
     set -q FZF_DEFAULT_COMMAND
-    or set -gx FZF_DEFAULT_COMMAND "fd -t f --hidden $exclude"
-
-    set -q FZF_CD_COMMAND
-    or set -gx FZF_CD_COMMAND "fd -Lt d $exclude"
-
-    set -q FZF_CD_WITH_HIDDEN_COMMAND
-    or set -gx FZF_CD_WITH_HIDDEN_COMMAND "fd -Lt d --hidden $exclude"
+    or set -gx FZF_DEFAULT_COMMAND "fd --type f --hidden $fzf_exclude"
 end
 
-# Options for PatrickF1/fzf.fish
-# These are passed to fd when searching directories
-set -gx fzf_fd_opts --hidden
+# For PatrickF1/fzf.fish
+set -gx fzf_fd_opts --hidden $fzf_exclude
 
 # Preview commands
 set -gx fzf_preview_dir_cmd eza --all --color=always
