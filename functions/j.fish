53dd4c15717b85d8441d6c4c4c5520a384af1938
Will Myers <will@withmartian.com>
2026-02-12 10:52:23 -0800

    cd to repo_root first

--- functions/j.fish
+++ functions/j.fish
@@ -1,4 +1,10 @@
 function j -d "cd with smarts"
+    set -l git_root (git rev-parse --show-toplevel 2>/dev/null)
+
+    if set -q git_root
+        cd $git_root
+    end
+
     if test -d "$argv[1]"
         cd $argv[1]
     else
